1343a2fcaaee8abab05cfa2c6d730510a7c7a7f7
Laura Cressman <laura.cressman@hey.com>
2021-01-07 10:30:26 -0700

    o face

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>QA Wolf :3</title>
+        <title>QA Wolf :o</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
